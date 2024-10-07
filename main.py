29a98867b3059dae5acbdf958430597472b4c4ff
Popitopi130 <j-emmanuel@hotmail.ca>
2024-10-06 20:36:56 -0400

    modified:   main.py

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ Programme d'analyse de pulse pour traitement FLASH
 import csv
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.integrate import trapezoid
+
+# TODO IN PART 2 OF PROJECT
+# Use pickle library for reading binary file from digitizer
 
 from typing import Final, List
 
@@ -36,8 +40,16 @@ def readData(file_name:str) -> List[np.ndarray]:
 
     return data
 
-def graphData(data:List[np.ndarray]):
-    return 0
+def graphData(data:List[np.ndarray], t_axis):
+    # TODO: Make it for the future pulse list thing (yk what you mean)
+    plt.figure(figsize=(10,6))
+    for pulse in data:
+        plt.plot(t_axis, pulse)
+    
+    plt.grid(True)
+    plt.xlabel("Temps (microseconde)")
+    plt.ylabel("Tension (V)")
+    plt.show()
 
 def levelData(data:List[np.ndarray]) -> List[np.ndarray]:
     levelData = []
@@ -54,8 +66,16 @@ def levelData(data:List[np.ndarray]) -> List[np.ndarray]:
         
         leveled_arr[np.abs(leveled_arr < threshold)] = 0
         levelData.append(leveled_arr)
+
+    # The data needs to be scaled down from 1000 to 0.1
+    levelData = [pulse / 10000 for pulse in levelData]
     
     return levelData
+
+
+def areaUnderCurve(data:List[np.ndarray], x_axis) -> List[np.ndarray]:
+    return [trapezoid(pulse, x_axis) for pulse in data] # This is in nC
+    
 
 def main() -> None:
     # TODO: Make it user chosen
@@ -69,17 +89,13 @@ def main() -> None:
     data = readData('SDataR_20240516_2.CSV')
     SAMPLE_SIZE: Final[int] = data[0].size
     STEPS: Final[int] = int(RECORD_LENGTH / SAMPLE_SIZE) #ns
-    t_axis = np.arange(0, RECORD_LENGTH, STEPS)
+    t_axis = np.linspace(0, RECORD_LENGTH / 1000, num=data[0].size)
     
     data = levelData(data)
     
-    t_axis = np.linspace(1,data[0].size,num=data[0].size)
-    plt.figure(figsize=(10,6))
-    for pulse in data:
-        plt.plot(t_axis, pulse)
+    graphData(data, t_axis)
     
-    plt.grid(True)
-    plt.show()
+    print(areaUnderCurve(data, t_axis))
     
     
 if __name__ == '__main__':
